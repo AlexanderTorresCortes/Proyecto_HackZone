7db668a9d8fba3d5fa49c00570d96d22eb1e04b6
AlexanderTorresCortes <168499649+AlexanderTorresCortes@users.noreply.github.com>
2025-12-10 18:43:59 -0600

    Add URL facade import in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\Equipo;
 use App\Models\EquipoMiembro;
 use App\Models\Evaluacion;
